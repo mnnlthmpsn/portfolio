33dab2f84d33c4b2ea943b5e81380aa62826f75d
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-08-02 10:06:23 +0000

    Under construction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 const IndexPage = () => {
     return (
         <div className="h-screen w-screen bg-primary flex items-center justify-center">
-            <p className="text-3xl font-bold text-white">This is my portfolio</p>
+            <p className="text-3xl font-bold text-white">Under Construction</p>
         </div>
     )
 }
